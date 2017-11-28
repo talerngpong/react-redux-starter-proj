0bdfe10360bc5c566dd50563386e02eb35ca3b89
Talerngpong Virojwutikul <talerngpong@hotmail.com>
2017-11-28 20:28:17 +0700

    Add linter with Airbnb style

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* global it: false */
+
+/**
+ neglect undefined global variables via => global var1: false, var2: false,...
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        document.getElementById('root'),
-    )
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  )
 }
 
 render(App)
 
 if (module.hot) {
-    module.hot.accept(
-        './App',
-        () => { render(App) }
-    )
+  module.hot.accept(
+    './App',
+    () => { render(App) },
+  )
 }
 
-registerServiceWorker();
+registerServiceWorker()
